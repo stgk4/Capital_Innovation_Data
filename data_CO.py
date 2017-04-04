13ce9182cf780d9ebbeebc477062ce288524ff12
Srinivas chakravarthi thandu <thandus@ociweb.com>
2017-04-04 14:46:22 -0500

    data updates

---
+++ data_CO.py
@@ -0,0 +1,29 @@
+import csv
+import itertools
+
+#------------------------------------
+def csv_reader(file_obj):
+	"""
+	Read a csv file
+	"""
+	reader = csv.reader(file_obj)
+
+	for row in itertools.islice(reader,10):
+		print(row)
+
+		#row1 = row.split('\t');
+		print(row.__len__())
+		#print(" ".join(row))  #row
+
+	#for i in range (2):
+		#print next(reader)
+		#print(i)
+
+	#for row in reader:
+	#	print(" ".join(row))
+
+#------------------------------------
+if __name__ == "__main__":
+	csv_path = "CI_Spring16.csv"
+	with open(csv_path, "r", encoding="utf8") as f_obj:
+		csv_reader(f_obj)
